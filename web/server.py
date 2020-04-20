7dd901a11c9c525b59776f50ad63df23bb5e333b
CarlosMerma08 <40005066+CarlosMerma@users.noreply.github.com>
2020-04-20 11:01:05 -0500

    2

--- web/server.py
+++ web/server.py
@@ -9,12 +9,20 @@ engine = db.createEngine()
 
 app = Flask(__name__)
 
+@app.route('/esprimo/<numero>')
+def esprimo(numero):
+    if(int(numero)>=1):
+        for i in range(2, int(numero)):
+            if (numero %i ==0):
+                print(numero," no es numero primo")
+            else:
+                print(numero," es primo")
+    else:
+        print(numero," no es un numero primo")
 
 @app.route('/static/<content>')
 def static_content(content):
     return render_template(content)
-
-
 
 
 if __name__ == '__main__':
